c955487768ae72643971ff43e2cce1bf573d6030
Tarmanjeet <tarmanjeetkaur@gmail.com>
2025-10-30 21:03:45 +0530

    cors error fixed

--- Backend/server.js
+++ Backend/server.js
@@ -12,27 +12,17 @@ const adminRouter = require("./routes/adminRoutes");
 const app = express();
 app.use(express.json());
 
-const cors = require('cors');
-
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://frontend-deploy-teal.vercel.app'
-];
+const cors = require("cors");
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: [
+    "http://localhost:5173",
+    "https://frontend-deploy-teal.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-
 
 connectToDB();
 
